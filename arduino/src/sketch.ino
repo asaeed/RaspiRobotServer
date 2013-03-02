3bbe9343269146a66984311e0e35171483443ed9
Ahmad Saeed <asaeed@gmail.com>
2013-03-01 22:36:09 -0500

    for aJSON, to avoid mem leak, don't do this: Serial.println(aJson.print(root)); - instead do this: char* json = aJson.print(root); Serial.println(json); free(json);

--- arduino/src/sketch.ino
+++ arduino/src/sketch.ino
@@ -5,8 +5,6 @@
 #include <Wire.h>
 #include <Servo.h>
 
-#include "MotorDriver.h"
-
 #define LED_PIN 13
 
 #define motorAPWM 11
@@ -23,8 +21,6 @@ MotorDriver rightMotor = MotorDriver(motorBIN1, motorBIN2, motorBPWM);
 HMC5883L compass = HMC5883L();
 Servo neckMotor;
 float neckAngle;
-char json[200];
-String jsonString;
 
 int msgByte= -1;         // incoming byte
 const int msgSize = 50;  // max message size
@@ -43,35 +39,76 @@ void setup() {
     neckMotor.write(neckAngle-1);
     
     Serial.println("setup done");
+    //freeMem("starting");
 }
 
 void loop() {
-
-  
   handleSerial();
-
 }
 
 void receiveData() {
+  freeMem("receive start");
   Serial.println(msgArray);
   aJsonObject* root = aJson.parse(msgArray);
-  aJsonObject* value = aJson.getObjectItem(root, "value");
-  neckAngle = value->valuefloat;
-  Serial.println(neckAngle);
+  aJsonObject* neck = aJson.getObjectItem(root, "neck");
+  neckAngle = neck->valuefloat;
+  //Serial.println(neckAngle);
   neckMotor.write(neckAngle);
+  
+  /*
+  int power = 60;
+  float x, y;
+  if (dataIn[5] == '2') {
+    if (x > .25 && x < .75 && y < .25) { // fwd
+      leftMotor.setSpeed(power);
+      rightMotor.setSpeed(power);
+      delay(100);
+      leftMotor.setSpeed(0);
+      rightMotor.setSpeed(0);
+    }
+    if (x > .25 && x < .75 && y > .75) { // back
+      leftMotor.setSpeed(-power);
+      rightMotor.setSpeed(-power);
+      delay(100);
+      leftMotor.setSpeed(0);
+      rightMotor.setSpeed(0);
+    }
+    if (x < .25 && y > .25 && y < .75) { // turn left
+      leftMotor.setSpeed(-power);
+      rightMotor.setSpeed(power);
+      delay(100);
+      leftMotor.setSpeed(0);
+      rightMotor.setSpeed(0);
+    }
+    if (x > .75 && y > .25 && y < .75) { // turn right
+      leftMotor.setSpeed(power);
+      rightMotor.setSpeed(-power);
+      delay(100);
+      leftMotor.setSpeed(0);
+      rightMotor.setSpeed(0);
+    }
+  }
+  */
+  
+  aJson.deleteItem(root);
+  freeMem("receive end");
 }
 
 void sendData() {
-  aJsonObject *root, *neck;
-  root = aJson.createObject();
   
   float heading = getHeading();
+  
+  freeMem("send start");
+  aJsonObject *root = aJson.createObject();
+  aJsonObject *neck;
   aJson.addItemToObject(root, "heading",  neck = aJson.createItem(heading)); 
   
-  Serial2.println(aJson.print(root));
+  char* json = aJson.print(root);
+  Serial2.println(json);
+  free(json);
   
   aJson.deleteItem(root);
-  
+  freeMem("send end");
   
 }
 
@@ -111,9 +148,61 @@ void setupHMC5883L(){
 }
 
 float getHeading(){
+ freeMem("before getHeading");
  MagnetometerScaled scaled = compass.ReadScaledAxis();
  float heading = atan2(scaled.YAxis, scaled.XAxis);
  if(heading < 0) heading += 2*PI;
  if(heading > 2*PI) heading -= 2*PI;
+ freeMem("after getHeading");
  return heading * RAD_TO_DEG;
+ 
+}
+
+
+
+//Code to print out the free memory
+
+extern unsigned int __heap_start;
+extern void *__brkval;
+
+/*
+ * The free list structure as maintained by the 
+ * avr-libc memory allocation routines.
+ */
+struct __freelist {
+  size_t sz;
+  struct __freelist *nx;
+};
+
+/* The head of the free list structure */
+extern struct __freelist *__flp;
+
+/* Calculates the size of the free list */
+int freeListSize() {
+  struct __freelist* current;
+  int total = 0;
+
+  for (current = __flp; current; current = current->nx) {
+    total += 2; /* Add two bytes for the memory block's header  */
+    total += (int) current->sz;
+  }
+  return total;
+}
+
+int freeMemory() {
+  int free_memory;
+
+  if ((int)__brkval == 0) {
+    free_memory = ((int)&free_memory) - ((int)&__heap_start);
+  } else {
+    free_memory = ((int)&free_memory) - ((int)__brkval);
+    free_memory += freeListSize();
+  }
+  return free_memory;
+}
+
+void freeMem(char* message) {
+  Serial.print(message);
+  Serial.print(":\t");
+  Serial.println(freeMemory());
 }
